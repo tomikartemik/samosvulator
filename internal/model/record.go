cc7f7d6421f5899a2041c488daaef8cc7bfd0efd
tomikartemik <artemules@mail.ru>
2025-03-11 00:26:10 +0300

    UPD: deleted new fields

--- internal/model/record.go
+++ internal/model/record.go
@@ -3,24 +3,21 @@ package model
 import "time"
 
 type Record struct {
-	ID              int       `gorm:"primaryKey;uniqueIndex;not null" json:"id"`
-	ExcavatorName   string    `gorm:"not null" json:"excavator_name"`
-	Date            time.Time `gorm:"not null" json:"date"`
-	Shift           string    `gorm:"not null" json:"shift"`
-	ShiftTime       int       `gorm:"not null" json:"shift_time"`
-	LunchTime       int       `gorm:"not null" json:"lunch_time"`
-	ETO             int       `gorm:"not null" json:"eto"`
-	ShiftChangeTime int       `gorm:"not null" json:"shift_change_time"`
-	LoadTime        float64   `gorm:"not null" json:"load_time"`
-	CycleTime       int       `gorm:"not null" json:"cycle_time"`
-	ApproachTime    int       `gorm:"not null" json:"approach_time"`
-	ActualTrucks    float64   `gorm:"not null" json:"actual_trucks"`
-	Productivity    int       `gorm:"not null" json:"productivity"`
-	RequiredTrucks  float64   `gorm:"not null" json:"required_trucks"`
-	PlanVolume      float64   `gorm:"not null" json:"plan_volume"`
-	ForecastVolume  float64   `gorm:"not null" json:"forecast_volume"`
-	Downtime        float64   `gorm:"not null" json:"downtime"`
-	UserID          int       `gorm:"not null" json:"user_id"`
+	ID             int       `gorm:"primaryKey;uniqueIndex;not null" json:"id"`
+	ExcavatorName  string    `gorm:"not null" json:"excavator_name"`
+	Date           time.Time `gorm:"not null" json:"date"`
+	Shift          string    `gorm:"not null" json:"shift"`
+	ShiftTime      int       `gorm:"not null" json:"shift_time"`
+	LoadTime       float64   `gorm:"not null" json:"load_time"`
+	CycleTime      int       `gorm:"not null" json:"cycle_time"`
+	ApproachTime   int       `gorm:"not null" json:"approach_time"`
+	ActualTrucks   float64   `gorm:"not null" json:"actual_trucks"`
+	Productivity   int       `gorm:"not null" json:"productivity"`
+	RequiredTrucks float64   `gorm:"not null" json:"required_trucks"`
+	PlanVolume     float64   `gorm:"not null" json:"plan_volume"`
+	ForecastVolume float64   `gorm:"not null" json:"forecast_volume"`
+	Downtime       float64   `gorm:"not null" json:"downtime"`
+	UserID         int       `gorm:"not null" json:"user_id"`
 }
 
 type RecordForAnalise struct {
